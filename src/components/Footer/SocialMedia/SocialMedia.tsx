2de1fbc40b83b535f2fd1c4c3def7e2b8cac5887
sriram <sriram250901@gmail.com>
2022-09-12 17:07:10 +0530

    linkedin

--- src/components/Footer/SocialMedia/SocialMedia.tsx
+++ src/components/Footer/SocialMedia/SocialMedia.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx'
-import { ReactComponent as FacebookLogo } from './facebook-logo.svg'
+import { ReactComponent as LinkedInLogo } from './linkdin.svg'
 import { ReactComponent as GithubLogo } from './github-logo.svg'
 
 export interface Props {
@@ -10,8 +10,8 @@ const SocialMedia = ({ className }: Props) => {
   return (
     <ul className={clsx(className, 'flex gap-2 fill-current')}>
       <li className="w-8">
-        <a href="https://www.facebook.com/sriram25">
-          <FacebookLogo />
+        <a href="https://www.linkedin.com/in/sriram-piratheepan-290876244/">
+          <LinkedInLogo />
         </a>
       </li>
       <li className="w-8">
